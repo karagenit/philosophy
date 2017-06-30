cb411bf0018156a2a14977a43c748297021ec02f
Caleb Smith <karagenit@outlook.com>
2017-06-30 19:28:58 +0000

    Add get_page Method

--- script.rb
+++ script.rb
@@ -3,8 +3,11 @@
 require 'net/http'
 require 'uri'
 
-uri = URI.parse("https://en.wikipedia.org")
-
-Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-    puts http.get("/wiki/Molecule").body
+def get_page(name) 
+    uri = URI.parse("https://en.wikipedia.org")
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+        return http.get("/wiki/#{name}").body
+    end
 end
+
+puts get_page("Molecule")
